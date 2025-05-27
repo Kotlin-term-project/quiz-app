2e568e8c46fbd1c0952020b4597e4021ccfa03ce
jiwon <wldnjs7805@naver.com>
2025-05-27 18:02:15 +0900

    feat: 정답률 화면 디자인

--- app/src/main/java/com/example/termproject/ShowRateActivity.kt
+++ app/src/main/java/com/example/termproject/ShowRateActivity.kt
@@ -1,5 +1,6 @@
 package com.example.termproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -32,6 +33,30 @@ class ShowRateActivity : AppCompatActivity() {
             loadFolderList() // 폴더 목록 불러오기
             updateFolderButton() // 버튼을 전체(n)으로 설정
             calculateRateCoroutine()  // 정답률 게산
+        }
+
+        // MainActivity 가는 버튼 구현
+        binding.mainBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // TestReadyActivity 가는 버튼 구현
+        binding.takeTestBtn.setOnClickListener {
+            val intent = Intent(this, TestReadyActivity::class.java)
+            startActivity(intent)
+        }
+
+        // ShowRateActivity 가는 버튼 구현
+        binding.rateBtn.setOnClickListener {
+            val intent = Intent(this, ShowRateActivity::class.java)
+            startActivity(intent)
+        }
+
+        // StudyTimerActivity 가는 버튼 구현
+        binding.timerBtn.setOnClickListener {
+            val intent = Intent(this, StudyTimerActivity::class.java)
+            startActivity(intent)
         }
     }
 
